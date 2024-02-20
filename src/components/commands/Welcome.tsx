5db550fe63edcc83a8f29c6d8ca2b3327b1b2946
Furqatbek Mashrabjonov <61729454+FurqatMashrabjonov@users.noreply.github.com>
2024-02-20 11:04:51 +0500

    initial commite

--- src/components/commands/Welcome.tsx
+++ src/components/commands/Welcome.tsx
@@ -1,60 +1,44 @@
 import {
-  Cmd,
-  HeroContainer,
-  Link,
-  PreImg,
-  PreName,
-  PreNameMobile,
-  PreWrapper,
-  Seperator,
+    Cmd,
+    HeroContainer,
+    Link,
+    PreImg,
+    PreName,
+    PreNameMobile,
+    PreWrapper,
+    Seperator,
 } from "../styles/Welcome.styled";
 
 const Welcome: React.FC = () => {
-  return (
-    <HeroContainer data-testid="welcome">
-      <div className="info-section">
-        <PreName>
-          {`        
-    _____       __     _   __      _            
-   / ___/____ _/ /_   / | / /___ _(_)___  ____ _
-   \\__ \\/ __ \`/ __/  /  |/ / __ \`/ / __  / __ \`/
-  ___/ / /_/ / /_   / /|  / /_/ / / / / / /_/ / 
- /____/\\__,_/\\___/ /_/ |_/\\__,_/_/_/ /_/\\__, /  
-                                       /____/   
+    return (
+        <HeroContainer data-testid="welcome">
+            <div className="info-section">
+                <PreName>
+                    {`
           `}
-        </PreName>
-        <PreWrapper>
-          <PreNameMobile>
-            {`
-    ____     __          
-   / __/__ _/ /_         
-  _\\ \\/ _ \`/ __/         
- /___/\\_,_/\\__/          
-    _  __     _          
-   / |/ /__ _(_)__  ___ _
-  /    / _ \`/ / _ \\/ _ \`/
- /_/|_/\\_,_/_/_//_/\\_, / 
-                  /___/  
- 
+                </PreName>
+                <PreWrapper>
+                    <PreNameMobile>
+                        {`
           `}
-          </PreNameMobile>
-        </PreWrapper>
-        <div>Welcome to my terminal portfolio.</div>
-        <Seperator>----</Seperator>
-        <div>
-          This project's source code can be found in this project's{" "}
-          <Link href="https://github.com/satnaing/terminal-portfolio">
-            GitHub repo
-          </Link>
-          .
-        </div>
-        <Seperator>----</Seperator>
-        <div>
-          For a list of available commands, type `<Cmd>help</Cmd>`.
-        </div>
-      </div>
-    </HeroContainer>
-  );
+                    </PreNameMobile>
+                </PreWrapper>
+                <div>Welcome to my terminal portfolio.</div>
+                <Seperator>----</Seperator>
+                <div>
+                    This project's source code can be found in this project's{" "}
+                    <Link href="https://github.com/satnaing/terminal-portfolio">
+                        GitHub repo
+                    </Link>
+                    .
+                </div>
+                <Seperator>----</Seperator>
+                <div>
+                    For a list of available commands, type `<Cmd>help</Cmd>`.
+                </div>
+            </div>
+        </HeroContainer>
+    );
 };
 
 export default Welcome;
